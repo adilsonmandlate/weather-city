a551e83fd320e6629e99ed0853c89a6d70749016
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-09-14 19:43:01 +0200

    feat: add utils functions

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -5,6 +5,7 @@ const colors = css`
   --colors-darkGreen: #16a085;
   --colors-midnight: #2c3e50;
   --colors-white: #fff;
+  --colors-concrete: #95a5a6;
   --colors-lightGrey: #ecf0f1;
   --colors-red: #e74c3c;
   --colors-heather: #b1becd;
